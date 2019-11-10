41cd151404c4033570afd000739f8c2367e88c63
sahib <sahib15khan@iitg.ernet.in>
2019-11-10 16:17:28 +0530

    Fixed Typo

--- vot-workspace/tracker_ADT.m
+++ vot-workspace/tracker_ADT.m
@@ -4,7 +4,7 @@
 % The human readable label for the tracker, used to identify the tracker in reports
 % If not set, it will be set to the same value as the identifier.
 % It does not have to be unique, but it is best that it is.
-tracker_label = ['ADT];
+tracker_label = ['ADT'];
 
 % For Python implementations we have created a handy function that generates the appropritate
 % command that will run the python executable and execute the given script that includes your
